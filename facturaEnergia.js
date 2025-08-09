fd67bf3a8c46f200f8f2ed3b4ea97ef8de0350d2
Deimer1991 <deimer.yav@hotmail.com>
2025-08-09 00:06:49 -0500

    Se crea funcion de tarifa mensual

--- facturaEnergia.js
+++ facturaEnergia.js
@@ -2,3 +2,11 @@ function consumoMensual (){
     let consumo = Number(prompt("Ingrese el consumo en de energia representado en Kwh: "))
       return consumo
 }
+let consumo = consumoMensual();
+
+function tarifaMensual (){
+    let tarifa = Number(prompt("Ingrese la tarifa por kwh: "))
+    return tarifa
+}
+
+const tarifa = tarifaMensual();
